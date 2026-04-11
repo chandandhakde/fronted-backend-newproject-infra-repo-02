f5b9014079c110bf045d22cff5590f4d01ff7218
chandandhakde <chandandhakde40@gmail.com>
2026-04-11 15:30:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,8 +5,8 @@ terraform {
        version = "~>6.32.0 "    }
   }
   backend "s3" {
-    bucket = "col-lms-project-2026-02"
-    key = "col-lms-project-2026-02/state.file/terraform.tfstate"
+    bucket = "col-lms-project-s3-bucket-01"
+    key = "col-lms-project-s3-bucket-01/state.file/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
     
